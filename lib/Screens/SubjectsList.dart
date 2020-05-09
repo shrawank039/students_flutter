0f5a9dcfabb37f4ffec0e25fe6faf01186e1542d
Shiv Kumar <shiukumar05@gmail.com>
2020-05-09 18:22:50 +0530

    fragments: Groupchat, privatechat, teacher list added

--- lib/Screens/SubjectsList.dart
+++ lib/Screens/SubjectsList.dart
@@ -1,108 +1,55 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'package:students/Fragments/GroupChat.dart';
+import 'package:students/Fragments/Privatechat.dart';
+import 'package:students/Fragments/TeachersList.dart';
 
 void main() {
   runApp(SubjectsList());
 }
 
 class SubjectsList extends StatelessWidget {
-  final timetable = [
-    '9am-10am',
-    '10am-11am',
-    '11am-12am',
-    '12am-1am',
-    '2am-3am',
-    '3am-4am',
-    '4am-5am'
-  ];
-  final subjects = [
-    'Mathematics',
-    'Science',
-    'History',
-    'Physics',
-    'Biology',
-    'Zoology',
-    'Health and Physical'
-  ];
-  final classStatus = [
-    'assets/images/completed_classes.png',
-    'assets/images/completed_classes.png',
-    'assets/images/completed_classes.png',
-    'assets/images/ongoing_classes.png',
-    'assets/images/pending_class.png',
-    'assets/images/pending_class.png',
-    'assets/images/pending_class.png'
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tabs App',
+      home: TabsApp(),
+    );
+  }
+}
+
+class TabsApp extends StatelessWidget {
+  List<Widget> containers = [
+    new GroupChat(),
+    new Privatechat(),
+    new TeachersList()
   ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
         appBar: AppBar(
-          title: Text("Subjects List"),
-        ),
-        body: Container(
-          child: ListView.builder(
-            itemCount: timetable.length,
-            scrollDirection: Axis.vertical,
-            itemBuilder: (BuildContext context, int position) {
-              return Container(
-                margin: EdgeInsets.only(left: 10.0, right: 10, top: 5),
-                child: GestureDetector(
-                  onTap: () {
-                    showToast('Position: $position');
-                  },
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: <Widget>[
-                          Text(
-                            timetable[position],
-                            style: TextStyle(
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey[500],
-                            ),
-                          ),
-                          Text(
-                            subjects[position],
-                            style: TextStyle(
-                              fontSize: 13,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.grey[700],
-                            ),
-                          ),
-                          Container(
-                            height: 25,
-                            width: 25,
-                            child: Image.asset(
-                              classStatus[position],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            },
+          title: Text('Chats'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Group Chat',
+              ),
+              Tab(
+                text: 'Private Chat',
+              ),
+              Tab(
+                text: 'Teachers',
+              ),
+            ],
           ),
+        ),
+        body: TabBarView(
+          children: containers,
         ),
       ),
     );
-  }
-
-  void showToast(message) {
-    Fluttertoast.showToast(
-        msg: message,
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.BOTTOM,
-        timeInSecForIos: 1,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0);
   }
 }
