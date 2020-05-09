e84098db81d8956da326ae347966f7ffe8102dbc
Microinstance <partha.mcr@gmail.com>
2020-05-09 19:55:50 +0530

    Chat code recover from git log

--- lib/GroupChat/ChatScreen.dart
+++ lib/GroupChat/ChatScreen.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import 'package:students/Utils/Message.dart';
 import 'package:flutter_socket_io/flutter_socket_io.dart';
